d7abd83c3ad707fc894683151a05688a737cdad4
mdziadosz <martyna.dziadosz.md@gmail.com>
2024-01-23 10:14:14 +0100

    Update streamlit_app.py
    changes in the breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit 
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled egg')
+
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado toast ')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
