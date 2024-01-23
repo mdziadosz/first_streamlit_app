ce2f7225b5b384a9c691dd006bf823b7afbd20b7
mdziadosz <martyna.dziadosz.md@gmail.com>
2024-01-23 10:16:39 +0100

    Update streamlit_app.py
    import of a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit 
+import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -7,7 +8,9 @@ streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocado toast ')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
