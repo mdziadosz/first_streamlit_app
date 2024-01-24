ce11ceb6c9f2b59772468842873954eba8a6a3ae
mdziadosz <martyna.dziadosz.md@gmail.com>
2024-01-24 11:37:19 +0100

    Update streamlit_app.py
    add CURRENT_ROLE()

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ import snowflake.connector
 # let's query out trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION(), CURRENT_ROLE()")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
