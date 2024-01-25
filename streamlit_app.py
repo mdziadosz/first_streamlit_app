4e7d12e0b29d923734fade934f0a313a1236bd69
mdziadosz <martyna.dziadosz.md@gmail.com>
2024-01-25 09:33:02 +0100

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about ?', 'Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about ?')
+  if not fruit_choice: 
+    streamlit.error("Please select a fruit to get information.")
+  else: 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # take the json version od the response and normalize it 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it the screen as a table 
+    streamlit.dataframe(fruityvice_normalized)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# take the json version od the response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
-
+'''
 # let's query out trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -57,4 +59,4 @@ add_new_fruit = streamlit.text_input('What fruit would you like to add ?')
 #streamlit.text(add_new_fruit)
 my_cur.execute("insert into fruit_load_list values (add_new_fruit)")
 
-
+'''
